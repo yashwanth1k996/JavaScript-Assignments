16766afe03eedf8bc27523c26bff4d9fa946d64c
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-04-29 00:15:58 +0530

    day12

---
+++ DAY-12/two.js
@@ -0,0 +1,19 @@
+function deepEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || typeof a != "object" || b == null || typeof b != "object")
+        return false;
+    var propsInA = 0,
+        propsInB = 0;
+    for (var prop in a)
+        propsInA += 1;
+    for (var prop in b) {
+        propsInB += 1;
+        if (!(prop in a) || !deepEqual(a[prop], b[prop]))
+            return false;
+    }
+    return propsInA == propsInB;
+}
+
+
+var obj = { one: 1, two: 2 };
+console.log(deepEqual({ one: 1 }, obj));
